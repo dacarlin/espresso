270c7a3c911a60e01847da5e208ad31210041943
Alex Carlin <alxcarln@gmail.com>
2023-08-19 14:46:11 -0700

    Add beginning of public API

--- coaster/main.py
+++ coaster/main.py
@@ -1,7 +1,23 @@
 def encode_sequence(protein_sequence, codon_table="sc"):
-    pass 
+    """Create a gene sequence from a protein sequence
+
+    Parameters
+    ----------
+    protein_sequence: str
+        Protein sequence as a string 
+    codon_table: str
+        The name of a codon model in Coaster
+
+    Examples
+    --------
+    An example of encoding a protein 
+
+    >>> encoded = encode_sequence("MMM")
+    """
+    return ""
+    
 
 def scrub_sequence(nucleotide_sequence, codon_table="sc"):
-    pass
+    return "" 
 
 
