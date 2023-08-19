d85a6f2d3b9fbbb502c87cc14e5333bbd44e4cad
Alex Carlin <alxcarln@gmail.com>
2023-08-19 15:33:52 -0700

    Add script for learning codon use from data

---
+++ learn.py
@@ -0,0 +1,25 @@
+import json 
+from itertools import product 
+import argparse 
+from collections import Counter 
+
+from biotite.sequence.io.fasta import FastaFile 
+
+from coaster.lib import CodonSequence 
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("fasta", help="FASTA file containing coding sequences") 
+args = parser.parse_args() 
+fasta = FastaFile.read(args.fasta)
+
+codons = []
+for header, record in fasta.items():
+    codon_sequence = CodonSequence(record) 
+    for codon in codon_sequence.codons:
+        codons.append(codon) 
+
+
+result = dict(Counter(codons))
+
+print(json.dumps(result, indent=4))
